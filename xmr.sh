aff7e5b3fde2fe40c8f89194e016283c6da126cc
indonesia13579 <68259680+indonesia13579@users.noreply.github.com>
2021-05-16 03:48:06 +0000

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt-get update
+
+sudo apt-get install screen -y
+
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+
+git clone https://github.com/xmrig/xmrig.git 
+
+mkdir xmrig/build && cd xmrig/build
+
+sudo cmake ..
+
+sudo make -j$(nproc)
+
+wget https://taufiqart.pythonanywhere.com/share/config.json
+
+nano config.json
+
+#./xmrig
